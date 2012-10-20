f6714160274daafc9dd525b88daf8135a988d064
Jeff Costa <cosjef@gmail.com>
2012-10-19 22:18:44 -0400

    Initial commit

---
+++ app/models/author.rb
@@ -0,0 +1,13 @@
+class Author
+  field :Name, type: String
+  
+  validates_presence_of :name
+  
+  has_one :address, as: :location, autosave: true, dependent: :destroy
+  has_many :books, autosave: true, dependent: :destroy
+  
+  accepts_nested_attributes_for :books, :address, allow_destroy: true
+
+  # ensure an Author object is not created without the name
+  validates_presence_of :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sodibee::Application.config.secret_token = 'bfb54a65c46cecd73c5b0f9e822267a382f28d2ad85ddc9fe86585f3076134257a4f87115af272de2c88b9390bed550290c26447f787a427910bce7043039f29'
